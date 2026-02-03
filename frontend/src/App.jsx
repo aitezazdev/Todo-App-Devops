cc3443b4607f950d619ca6b040d32a4fbaf88537
aitezazdev <aitezazsikandar@gmail.com>
2026-02-03 18:53:21 +0500

    change btn color

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -92,7 +92,7 @@ function App() {
           />
           <button
             type="submit"
-            className="bg-green-500 text-white px-4 py-2 rounded-r hover:bg-green-600"
+            className="bg-blue-500 text-white px-4 py-2 rounded-r hover:bg-blue-600"
           >
             Add
           </button>
