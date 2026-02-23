bf70b28c850de6bc088263b8c7d3ab47f09fa5a7
aitezazdev <aitezazsikandar@gmail.com>
2026-02-24 01:55:38 +0500

    add ingress config and persistent storage

--- backend/app.js
+++ backend/app.js
@@ -20,6 +20,6 @@ app.use('/api', router);
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+app.listen(PORT, "0.0.0.0", () => {
   console.log(`Server is running on port ${PORT}`);
 });
